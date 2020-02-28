6eb5b4ba0338e4284d297de73da7e97a6867f834
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-02-28 10:34:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.1ce5560d4355366c792c14b50e01c009.js"
 );
 
 self.addEventListener('message', (event) => {
